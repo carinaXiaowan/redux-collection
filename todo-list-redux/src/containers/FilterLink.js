499fa10ca9a627146f38c81e6b1f713ff8f0c9cb
xiaowan.chen <xiaowan.chen@weimob.com>
2018-12-19 18:04:11 +0800

    todolist

---
+++ todo-list-redux/src/containers/FilterLink.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from '../component/Link'
+
+const mapStateToProps = (state, ownProps) => {
+  return (
+    {
+      active: ownProps.filter === state.visibilityFilter
+    }
+  )
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link)
